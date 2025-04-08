ae1f1c24dbccde0844711c6530605375f8163233
toinan.beroud@gmail.com <toinan.beroud@gmail.com>
2025-04-08 15:10:34 +0200

    maj min

--- main.R
+++ main.R
@@ -13,10 +13,10 @@ library(mapsf)
 ###############################################################################
 ############################################################### FONDS D'ALIETTE
 
-# Recuperation des iris
-url <- "https://sharedocs.huma-num.fr/wl/?id=Ho4XQWuOU2DLt7ppdE1Set3x3gsL3QbO&mode=grid&download=1"
-iris <- st_read(url)
-
+# # Recuperation des iris
+# url <- "https://sharedocs.huma-num.fr/wl/?id=Ho4XQWuOU2DLt7ppdE1Set3x3gsL3QbO&mode=grid&download=1"
+# iris <- st_read(url)
+# 
 # # Table de passage vers les iris regroupes
 # load("C:/Users/Antoine Beroud/Desktop/rexplo/input/mar/donnees/AR02_maille_IRISr.RData")
 # tabl <- d.irisr.pass
@@ -39,8 +39,35 @@ data$IRISr <- ifelse(nchar(data$IRISr) == 8,
                      data$IRISr)
 
 # Creation du fond et des zooms -----------------------------------------------
-fond <- iris
+# Ouverture du fichier des iris
+iris <- st_read("input/iris.gpkg")
+iris <- iris[, c(1,2,7)]
+colnames(iris) <- c("IRIS_CODE", "IRIS_LIB", "P21_POP", "geometry")
+st_geometry(iris) <- "geometry"
+iris <- st_transform(iris, 2154)
 
+# Ouverture du fichier avec toutes les iris pour ajouter Mayotte
+mayo <- st_read("C:/Users/Antoine Beroud/Desktop/rexplo/input/mar/donnees/shapefiles/AR01_sf_irisf.shp")
+mayo <- st_as_sf(mayo)
+mayo <- st_transform(mayo, 2154)
+
+mayo <- mayo[grepl("^976", mayo$IRISF_CODE), ]
+
+mayo$P21_POP <- NA
+mayo$P21_POP <- as.numeric(mayo$P21_POP)
+
+mayo <- mayo[, c(1,2,7)]
+colnames(mayo) <- c("IRIS_CODE", "IRIS_LIB", "P21_POP", "geometry")
+st_geometry(mayo) <- "geometry"
+mayo <- st_transform(mayo, 2154)
+
+# Collage des deux data.frames
+fond <- rbind(iris, mayo)
+
+# Repositionnement des geometries des DROM
+fond <- create_fond(fond, id = "IRIS_CODE")
+
+# Creation des zooms
 zoom_created <- create_zoom(fond = fond,
                             villes = c("Paris", "Marseille", "Lyon", "Toulouse", "Nantes", "Montpellier",
                                        "Bordeaux", "Lille", "Rennes", "Reims", "Dijon","Strasbourg",
@@ -52,12 +79,15 @@ zoom_created <- create_zoom(fond = fond,
 zooms <- zoom_created$zooms
 labels <- zoom_created$labels
 
+# Simplification des geometries du fond de carte principal
 fond <- simplify_geom(fond, keep = 0.1)
 
+# Jointure entre le fond et les donnees
 fondata <- merge_fondata(data = data,
                          fond = fond,
                          zoom = zooms,
                          id = c("IRISr", "IRIS_CODE"))
+
 
 palette <- c("1" = "#94282f",
              "2" = "#e40521",
@@ -82,8 +112,7 @@ mf_map(fondata,
        pal = palette,
        border = NA)
 
-
-st_write(fondata, "test.gpkg")
+st_write(fond, "testeoeo.gpkg")
 
 
 
